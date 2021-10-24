2ee2172e0bf55f74a3347c2d9409e923aff34b31
zloyramazan <ramazan-rasulov-2016@mail.ru>
2021-10-24 12:38:14 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -43,6 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 	let gain = new Decimal(1)
 	if (hasUpgrade('d', 11)) gain = gain.times(2)
+	if (hasUpgrade('d', 12)) gain = gain.times(upgradeEffect('d', 12))
 	return gain
 }
 
