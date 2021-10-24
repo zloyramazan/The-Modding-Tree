3784fb4fea67e7f87e1d47879434737ac04b88c6
zloyramazan <ramazan-rasulov-2016@mail.ru>
2021-10-24 13:13:10 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -19,8 +19,8 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0.1</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Added dob layer.<br>
+		- Added 3 upgrades to dob layer.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
